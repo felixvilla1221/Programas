5688edc276b51886de8cc9cd16c7f4e74859faa2
Felix Villamizar <felixvilla51@gmail.com>
2021-10-30 09:55:22 -0500

    Primer Commit de Prueba

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
@@ -1,10 +1,21 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/app")
 public class HomeController {
+	
+	
+	
+	
+@GetMapping("home")
+public String home(Model model) {
+model.addAttribute("Hola", "Titulo");	
+return null;	
+}
 
 }
