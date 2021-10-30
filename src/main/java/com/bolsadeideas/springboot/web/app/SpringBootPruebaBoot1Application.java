6670e63ff2c2a09072e87946ee049d232bdafae5
felixvilla1221 <86843683+felixvilla1221@users.noreply.github.com>
2021-10-30 09:17:04 -0500

    PROYECTO DE PRUEBAS
    PRACTICANDO GIT

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/SpringBootPruebaBoot1Application.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPruebaBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPruebaBoot1Application.class, args);
+	}
+
+}
